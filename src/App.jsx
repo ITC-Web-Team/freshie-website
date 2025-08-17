686421867b134cb7417f1f746b67b2ed6c7a336e
Harshitha-282007 <harshitharamagiri282007@gmail.com>
2025-08-17 20:07:48 +0530

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CouncilPage from './pages/CouncilPage';
 import ClubPage from './pages/ClubPage';
+import Footer from './components/Footer';
 
 function App () {
   return (
+    <>
     <Router>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='council/:councilId' element={<CouncilPage />} />
         <Route path='council/:councilId/club/:clubId' element={<ClubPage />} />
       </Routes>
+      <Footer/>
     </Router>
+    </>
   )
 }
 
